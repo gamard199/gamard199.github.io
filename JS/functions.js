b6a197f9024b26e217df75a4b0c87dc427aa06e1
Geovanny <geoconsultores.eu@gmail.com>
2021-03-02 12:11:05 -0500

    Inicio

---
+++ JS/functions.js
@@ -0,0 +1,40 @@
+alert("Hola Mundo");
+
+/*var nombre = "Juan";
+var apellido = "Perez";
+var edad = 30;
+
+alert(nombre);*/
+
+/*
+
+alert(5+4*8);
+
+alert((5+4)*8);
+
+*/
+
+var precioUnidad, cantidad, total; // Declaracion de 3 variables en una sentencia
+
+precioUnidad = 100; // Inicializacion de variable
+cantidad = 5;
+total = precioUnidad * cantidad;
+
+alert("El valor total de la compra es: " + "$ " + total);
+
+var camisetas = "5";
+var pantalones = 5;
+var totalArticulos = Number(camisetas) + pantalones;
+
+alert(totalArticulos);
+
+var articulos = [];
+
+articulos.push(prompt("Ingrese articulo que quiere comprar: "));
+
+articulos.push(prompt("Ingrese articulo que quiere comprar: "));
+
+alert("Articulos adquiridos: " + articulos[articulos.length-1] + ", " + articulos[articulos.length-2]);
+
+
+
